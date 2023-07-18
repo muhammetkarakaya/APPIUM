12e8c9d9d2ba35c0f73e256bcd0fd46dd77ac206
muhammetkarakaya <karakayaatc@gmail.com>
2023-07-18 20:46:28 +0200

    6. update

--- src/test/java/tests/test02/AppYukleme.java
+++ src/test/java/tests/test02/AppYukleme.java
@@ -19,7 +19,7 @@ public class AppYukleme {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\msas\\IdeaProjects\\APPIUM-T113\\Apps\\arabam.com_4.8.0_Apkpure.apk");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\msas\\IdeaProjects\\APPIUM-T113\\Apps\\Kiwi.com - Book Cheap Flights_2023.14.0_Apkpure (1).apk");
 
         driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
