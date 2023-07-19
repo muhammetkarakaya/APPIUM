c75b47d7170328290d2e23c00d87d975934ee312
muhammetkarakaya <karakayaatc@gmail.com>
2023-07-19 09:42:28 +0200

    8. update

--- src/test/java/pages/KiwiPage.java
+++ src/test/java/pages/KiwiPage.java
@@ -1,6 +1,10 @@
 package pages;
 
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.Driver;
 
@@ -8,7 +12,37 @@ public class KiwiPage {
     public KiwiPage(){
         PageFactory.initElements((WebDriver) Driver.getAndroidDriver(),this);
 
+    }
+
+    @FindBy(xpath = "//*[@text='Continue as a guest']")
+    public WebElement misafir;
+    @FindBy(xpath ="//*[@class='android.widget.EditText']" )
+    public WebElement departureBox;
+
+    @FindBy(xpath = "//*[@text='Choose']")
+    public WebElement chooseButton;
+
+    @FindBy(id = "com.skypicker.main:id/saveButton")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "(//*[@class='android.widget.TextView'])[12]")
+    public WebElement fiyat;
 
 
+
+
+
+    public void ucAdimGecme(int iBaslangic,int iBitis,int xKoordinat,int yKoordinat,int bekleme) throws InterruptedException {
+        TouchAction touchAction=new TouchAction<>(Driver.getAndroidDriver());
+        for (int i=iBaslangic; i<iBitis; i++){
+            touchAction.press(PointOption.point(xKoordinat,yKoordinat)).release().perform();
+            Thread.sleep(bekleme);
+        }
+    }
+
+    public void koordinatTiklama(int xKoordinat,int yKoordinat,int bekleme) throws InterruptedException {
+        TouchAction touchAction=new TouchAction<>(Driver.getAndroidDriver());
+        touchAction.press(PointOption.point(xKoordinat,yKoordinat)).release().perform();
+        Thread.sleep(bekleme);
     }
 }
--- src/test/java/tests/test04/KiwiCom.java
+++ src/test/java/tests/test04/KiwiCom.java
@@ -1,13 +1,18 @@
 package tests.test04;
 
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.offset.PointOption;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.KiwiPage;
 import utils.Driver;
 
 public class KiwiCom {
     AndroidDriver<AndroidElement>driver = Driver.getAndroidDriver();
+    TouchAction touchAction=new TouchAction<>(driver);
+    KiwiPage kiwiPage = new KiwiPage();
     // uygulamanin yuklendigi dogrulanir
     // uygulamanin basariyla acildigi dogrulanir
     // misafir olarak devam et e tiklanir
@@ -22,14 +27,44 @@ public class KiwiCom {
     // gelen bilet fiyati kaydedilir ve kullanicin telefonuna sms olarak gonderilir
 
     @Test
-    public void kiwiTest(){
+    public void kiwiTest() throws InterruptedException {
         // uygulamanin yuklendigi dogrulanir
         Assert.assertTrue(driver.isAppInstalled("com.skypicker.main"));
         // uygulamanin basariyla acildigi dogrulanir
-        AndroidElement guest=driver.findElementByXPath("//*[@text='Continue as a guest']");
-        Assert.assertTrue(guest.isDisplayed());
+        //AndroidElement guest=driver.findElementByXPath("//*[@text='Continue as a guest']");
+        Assert.assertTrue(kiwiPage.misafir.isDisplayed());
         // misafir olarak devam et e tiklanir
+        kiwiPage.misafir.click();
         // ardinda gelecek olan 3 adimada yesil butona basilarak devam edilir
+        //koordinat 538,1686
+
+        for (int i = 0; i < 3; i++) {
+
+            touchAction.press(PointOption.point(538,1686)).release().perform();
+            Thread.sleep(1000);
+
+        }
+        // Trip type,one way olarak secilir
+        Thread.sleep(2000);
+        kiwiPage.koordinatTiklama(290,627,1000);
+        Thread.sleep(1000);
+        kiwiPage.koordinatTiklama(552,1448,1000);
+
+        // kalkis ulkesi secenegine tiklanir ve default olan ulke kaldirilir
+        kiwiPage.koordinatTiklama(502,780,1000);
+        kiwiPage.koordinatTiklama(1016,135,1000);
+
+        // kalkis yapilacak ulke/sehir girilir ve sec e tiklanir
+        if (driver.isKeyboardShown()){
+            driver.getKeyboard().pressKey("Istanbul");
+        } else {
+            kiwiPage.departureBox.sendKeys("Atina");
+        }
+        Thread.sleep(1000);
+        kiwiPage.koordinatTiklama(493,294,1000);
+
+        kiwiPage.chooseButton.click();
+
 
     }
 
